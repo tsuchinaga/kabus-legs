d33af2be7ed903bcaad49fe809354b086041838e
tsuchinaga <woocun@gmail.com>
2020-08-27 19:10:07 +0900

    ストアからトークンを取得する処理を追加

--- infra/store/setting.go
+++ infra/store/setting.go
@@ -4,7 +4,8 @@ import "sync"
 
 // setting - 設定ストア
 type setting struct {
-	password string
+	password string // パスワード
+	token    string // トークン
 	mtx      sync.Mutex
 }
 
@@ -20,4 +21,11 @@ func (s *setting) SetPassword(password string) {
 	s.mtx.Lock()
 	defer s.mtx.Unlock()
 	s.password = password
+}
+
+// GetToken - トークンを取得する
+func (s *setting) GetToken() string {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+	return s.token
 }
--- infra/store/setting_test.go
+++ infra/store/setting_test.go
@@ -58,3 +58,26 @@ func Test_setting_SetPassword(t *testing.T) {
 		})
 	}
 }
+
+func Test_setting_GetToken(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name  string
+		token string
+		want  string
+	}{
+		{name: "tokenを返す", token: "token", want: "token"},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			store := &setting{token: test.token}
+			got := store.GetToken()
+			if !reflect.DeepEqual(test.want, got) {
+				t.Errorf("%s error\nwant: %+v\ngot: %+v\n", t.Name(), test.want, got)
+			}
+		})
+	}
+}
